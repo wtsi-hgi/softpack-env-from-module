de63a1987ac2679f671e89d86034cf5bafadc407
Michael Woolnough <mw31@sanger.ac.uk>
2023-09-12 11:55:58 +0100

    Add flag to update existing environment instead of creating new one.

--- addModuleToSoftpack.sh
+++ addModuleToSoftpack.sh
@@ -3,11 +3,25 @@
 set -euo pipefail;
 
 help() {
-	echo "Usage: $0 path_to_module_file module_load_path environment_path" >&2;
+	echo "Usage: $0 [-u] path_to_module_file module_load_path environment_path" >&2;
+	echo;
+	echo "  -u: Update existing environment instead of creating it."
 	echo;
 	echo "The SOFTPACK_CORE_URL environmental variable must be set to the graphql endpoint for Softpack Core."
 	exit 1;
 }
+
+declare mutationName="createFromModule";
+declare successType="CreateEnvironmentSuccess";
+declare failType="EnvironmentAlreadyExistsError";
+
+if [ $# -gt 0 -a $1 = "-u" ]; then
+	mutationName="updateFromModule";
+	successType="UpdateEnvironmentSuccess";
+	failType="EnvironmentNotFoundError";
+
+	shift;
+fi;
 
 if [ $# -lt 3 ]; then
 	help;
@@ -34,18 +48,18 @@ module load "$modPath" &> /dev/null || {
 
 declare graphQL="$(while read line; do echo -n "$line\n";done <<HEREDOC
 mutation (\\\$file: Upload!, \\\$modulePath: String!, \\\$envPath: String!) {
-	createFromModule(
+	$mutationName(
 		file: \\\$file
 		modulePath: \\\$modulePath
 		environmentPath: \\\$envPath
 	) {
-		... on CreateEnvironmentSuccess {
+		... on $successType {
 			message
 		}
 		... on InvalidInputError {
 			message
 		}
-		... on EnvironmentAlreadyExistsError {
+		... on $failType {
 			message
 			path
 			name
